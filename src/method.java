c32bc11bf3a350418fce6a3408a034d4a9c179a9
Kendrick Torrance <torraken000@warren.k12.in.us>
2024-08-19 12:43:53 -0400

    Initial commit

---
+++ src/method.java
@@ -0,0 +1,24 @@
+public class assignment{
+
+    public static void main(String[] args) {
+      checkNumber(2);
+      checkNumber(-3);
+      checkNumber(0);
+
+
+    }
+
+    public static void checkNumber(int number) {
+
+        if (number > 0){
+            System.out.println("Positive");
+        }
+        else if (number < 0) {
+            System.out.println("Negative");
+        }
+        System.out.println("Zero");
+    }
+
+
+
+}
